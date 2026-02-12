6e652e3fe1ca755b950aefd335c2dc9f21c41556
Hinata <os53794330@gmail.com>
2026-02-12 09:05:10 +0100

    implement cateories and all coins tables

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import React, { Suspense } from 'react'
 import CoinOverview from './components/home/CoinOverview'
 import TrendingCoins from './components/home/TrendingCoins'
-import { CoinOverviewFallback, TrendingCoinsFallback } from './components/home/fallback'
+import { CoinOverviewFallback, TrendingCoinsFallback, CategoriesFallback } from './components/home/fallback'
+import Categories from './components/home/Categories'
 
 const page = async () => {
   return <main className="main-container">
@@ -17,7 +18,9 @@ const page = async () => {
     </section>
 
     <section className="w-full mt-7 space-y-4">
-      <p>Categories</p>
+      <Suspense fallback={<CategoriesFallback />}>
+        <Categories />
+      </Suspense>
     </section>
 
   </main>
