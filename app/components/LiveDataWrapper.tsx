928d06b5a4304bb76d21517fb1058abc27e9b7d7
Hinata <os53794330@gmail.com>
2026-02-27 13:27:05 +0100

    Fixed stuff

--- app/components/LiveDataWrapper.tsx
+++ app/components/LiveDataWrapper.tsx
@@ -48,18 +48,18 @@ const LiveDataWrapper = ({ children, coinId, poolId, coin, coinOHLCData }: LiveD
 
     return (
         <section id="live-data-wrapper">
-            <CoinHeader 
-            name={coin.name}
-            image={coin.image.large}
-            livePrice={price?.usd ?? coin.market_data.current_price.usd}
-            livePriceChangePercentage24h={
-                price?.change24h ?? coin.market_data
-                 .price_change_percentage_30d_in_currency.usd
-            }
-            priceChangePercentage30d={coin.market_data
-                 .price_change_percentage_30d_in_currency.usd
-            }
-            priceChange24h={coin.market_data.price_change_24h_in_currency.usd}
+            <CoinHeader
+                name={coin.name}
+                image={coin.image.large}
+                livePrice={price?.usd ?? coin.market_data.current_price.usd}
+                livePriceChangePercentage24h={
+                    price?.change24h ?? coin.market_data
+                        .price_change_percentage_30d_in_currency.usd
+                }
+                priceChangePercentage30d={coin.market_data
+                    .price_change_percentage_24h_in_currency.usd
+                }
+                priceChange24h={coin.market_data.price_change_24h_in_currency.usd}
             />
             <Separator className="divider" />
 
